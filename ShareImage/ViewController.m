d32c65d883562cc2b4894ea66de46ff162c06192
DaiSuke <feng.daisuke@gmail.com>
2017-02-16 11:01:23 +0800

    Initial Commit

---
+++ ShareImage/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ShareImage
+//
+//  Created by DaiSuke on 2017/2/16.
+//  Copyright © 2017年 DaiSuke. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ ShareImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ShareImage
+//
+//  Created by DaiSuke on 2017/2/16.
+//  Copyright © 2017年 DaiSuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
