5f860633d19d5bccf726398d865da7c9d5c17b77
fengty <feng.daisuke@gmail.com>
2017-03-10 18:22:04 +0800

    导航栏微调

--- ShareImage/Classes/Module/HomeModule/DHomeViewController.m
+++ ShareImage/Classes/Module/HomeModule/DHomeViewController.m
@@ -298,7 +298,7 @@
         _tableView.delegate = self;
         _tableView.tableFooterView = [UIView new];
         _tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
-        _tableView.backgroundColor = DSystemColorGray;
+        _tableView.backgroundColor = [UIColor setHexColor:@"#f3f3f3"];
     }
     return _tableView;
 }
