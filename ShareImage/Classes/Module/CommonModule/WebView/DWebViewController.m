f0dadd916d293660d1af3148471753ce558e4ed7
FTY <feng.daisuke@gmail.com>
2018-05-07 15:19:22 +0800

    UIwebview替换wkwebview

--- ShareImage/Classes/Module/CommonModule/WebView/DWebViewController.m
+++ ShareImage/Classes/Module/CommonModule/WebView/DWebViewController.m
@@ -7,16 +7,14 @@
 //
 
 #import "DWebViewController.h"
-#import <WebKit/WebKit.h>
 #import "DShareManager.h"
 
-@interface DWebViewController ()<WKNavigationDelegate, WKUIDelegate>
-@property (nonatomic, strong) WKWebView *webView;
-@property (nonatomic, strong) UIProgressView *progressView;
+@interface DWebViewController ()<UIWebViewDelegate>
+@property (nonatomic, strong) UIWebView *webView;
 @property (nonatomic, strong) UIButton *closeBtn;
 
 @end
-#warning 出现白屏问题，查找了相关资料也不行，哪位大佬能解决的，告知一声，谢谢
+
 @implementation DWebViewController
 
 
@@ -26,7 +24,6 @@
     if (self) {
         self.urlStr = [url copy];
         self.type = WKWebViewLoadURLType;
-//        [self startLoad];
     }
     return self;
 }
@@ -36,7 +33,6 @@
     if (self) {
         self.type = type;
         self.urlStr = [url copy];
-//        [self startLoad];
     }
     return self;
 }
@@ -100,12 +96,10 @@
  *  例如：完整的网页可能包含广告部分，但是又不想要广告，所以我们可以加载整个网页，然后截取想要的部分，再加载
  */
 - (void)loadLineHTML{
-    
     if (self.urlStr.length > 0) {
         NSString *lineHtmlStr = [[NSString alloc] initWithContentsOfURL:[NSURL URLWithString:self.urlStr] encoding:NSUTF8StringEncoding error:nil];
         [self.webView loadHTMLString:lineHtmlStr baseURL:[NSURL URLWithString:self.urlStr]];
     }
-    
 }
 
 /**
@@ -144,7 +138,7 @@
     NSURLSession *session = [NSURLSession sharedSession];
     NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         @strongify(self)
-        [self.webView loadData:data MIMEType:response.MIMEType characterEncodingName:@"UTF8" baseURL:[NSURL URLWithString:@""]];
+        [self.webView loadData:data MIMEType:response.MIMEType textEncodingName:@"UTF8" baseURL:[NSURL URLWithString:@""]];
     }];
     [dataTask resume];
 }
@@ -154,31 +148,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-#warning load
+    self.view.backgroundColor = DSystemColorWhite;
     // 初始化导航栏
     [self setupNav];
     
-    [self.view addSubview:self.progressView];
     [self.view addSubview:self.webView];
     
-    self.progressView.sd_layout
-    .topSpaceToView(self.view, self.navBarHeight)
-    .leftSpaceToView(self.view, 0)
-    .rightSpaceToView(self.view, 0)
-    .heightIs(3);
-//    [self.progressView setFrame:0 y:self.navBarHeight w:self.view.width h:3];
-    
-    
-    DLogRect(self.view.frame);
-    DLog(@"%@", @(self.navBarHeight));
-    
     self.webView.sd_layout
-    .topSpaceToView(self.progressView, 0)
+    .topSpaceToView(self.navigationController.navigationBar, 0)
     .leftSpaceToView(self.view, 0)
     .rightSpaceToView(self.view, 0)
     .bottomSpaceToView(self.view, 0);
     
-//    [self.webView setFrame:0 y:self.navBarHeight w:self.view.width h:200];
     [self startLoad];
 }
 
@@ -232,14 +213,11 @@
 - (void)navigationBarDidClickNavigationBtn:(UIButton *)navBtn isLeft:(BOOL)isLeft{
     if (!isLeft) {
         @weakify(self);
-        [DShareManager  shareUrlForAllPlatformByTitle:self.webView.title content:@"" shareUrl:self.urlStr image:nil customPlatforms:@[@{@"platformIcon":@"common_refresh", @"platformName":@"刷新"}] parentController:self eventBlock:^(NSInteger index, NSDictionary *userInfo) {
+        [DShareManager  shareUrlForAllPlatformByTitle:[self.webView stringByEvaluatingJavaScriptFromString:@"document.title"] content:@"" shareUrl:self.urlStr image:nil customPlatforms:@[@{@"platformIcon":@"common_refresh", @"platformName":@"刷新"}] parentController:self eventBlock:^(NSInteger index, NSDictionary *userInfo) {
             DLog(@"index = %@, userInfo = %@", @(index), userInfo);
             @strongify(self);
             [self.webView reload];
         }];
-        //        UIActivity *activity = [[UIActivity alloc] init];
-        //
-        //        UIActivityViewController *viewController = [[UIActivityViewController alloc] initWithActivityItems:@[] applicationActivities:@[]];
         
         return;
     }
@@ -247,45 +225,27 @@
 }
 
 #pragma mark - WKNavigationDelegate
-/*
- 这个是网页加载完成，导航的变化
- 主意：这个方法是当网页的内容全部显示（网页内的所有图片必须都正常显示）的时候调用（不是出现的时候就调用），，否则不显示，或则部分显示时这个方法就不调用。
- */
--(void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation{
-    DLog(@"加载完成调用");
-    // 获取加载网页的标题
-    self.title = self.webView.title;
+#pragma mark - UIWebViewDelegate
+- (void)webViewDidStartLoad:(UIWebView *)webView{
+    self.title = @"正在加载...";
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+}
+
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView{
+    self.title = [webView stringByEvaluatingJavaScriptFromString:@"document.title"];
     [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
     self.closeBtn.hidden = !webView.canGoBack;
     
     [self removeNoNetworkAlertView];
     [self removeNetworkErrorReloadView];
+
 }
 
-
-/**
- 开始加载
- */
--(void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation{
-    DLog(@"开始加载的时候调用。。");
-    //开始加载的时候，让加载进度条显示
-    self.progressView.hidden = NO;
-    self.title = @"正在加载...";
-    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
-    
-    /**
-     内容返回时调用
-     */
-}
--(void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation{
-    DLog(@"当内容返回的时候调用");
-}
-
-- (void)webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error{
-    DLog(@"加载失败--%@", error);
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error{
     [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
-    self.title = webView.title;
-    if (error.code == -999) {
+    self.title = [webView stringByEvaluatingJavaScriptFromString:@"document.title"];
+    if (error.code == -999 || error.code == 102) {
         return;
     }
     [self proccessWebViewLoadFail];
@@ -299,95 +259,26 @@
     self.noNetworkDelegate = self;
 }
 
-
 /**
  点击刷新
  */
-- (void)pressNoNetworkBtnToRefresh
-{
+- (void)pressNoNetworkBtnToRefresh{
     [self removeNoNetworkAlertView];
     [self removeNetworkErrorReloadView];
     [self startLoad];
 }
 
 
-/**
- 计算webView进度条
- */
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
-    if (object == self.webView && [keyPath isEqualToString:@"estimatedProgress"]) {
-        CGFloat newprogress = [[change objectForKey:NSKeyValueChangeNewKey] doubleValue];
-        if (newprogress == 1) {
-            self.progressView.hidden = YES;
-            [self.progressView setProgress:0 animated:NO];
-            [UIView animateWithDuration:0.2 animations:^{
-                CGRect rect = self.webView.frame;
-                rect.origin.y = self.navBarHeight;
-                self.webView.frame = rect;
-            }];
-        }else {
-            self.progressView.hidden = NO;
-            [self.progressView setProgress:newprogress animated:YES];
-            [UIView animateWithDuration:0.2 animations:^{
-                CGRect rect = self.webView.frame;
-                rect.origin.y = self.navBarHeight+2.5;
-                self.webView.frame = rect;
-            }];
-        }
-    }
-}
-
-// 取消监听
-- (void)dealloc {
-    [self.webView removeObserver:self forKeyPath:@"estimatedProgress"];
-}
-
-
 #pragma mark - get & set
-- (WKWebView *)webView{
+- (UIWebView *)webView{
     if (!_webView) {
-        // 配置文件
-        WKWebViewConfiguration *configuration = [[WKWebViewConfiguration alloc] init];
-        
-        if (IOS9) {
-            // 允许视频播放
-            configuration.allowsAirPlayForMediaPlayback = YES;
-            // 允许图片播放
-            configuration.allowsPictureInPictureMediaPlayback = YES;
-        }
-        
-        // 允许在线播放
-        configuration.allowsInlineMediaPlayback = YES;
-        
-        // 循序与网页交互
-        configuration.selectionGranularity = YES;
-        
-        // 是否支持记忆读取
-        configuration.suppressesIncrementalRendering = YES;
-        
-        _webView = [[WKWebView alloc] initWithFrame:CGRectZero configuration:configuration];
-        
-        // 设置代理
-        _webView.UIDelegate = self;
-        _webView.navigationDelegate = self;
-        
-        [_webView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:nil];
-        // 开启手势触摸
-        _webView.allowsBackForwardNavigationGestures = YES;
+        _webView = [[UIWebView alloc] init];
+        _webView.backgroundColor = DSystemColorWhite;
+        _webView.delegate = self;
         // 适应设定的尺寸
         [_webView sizeToFit];
     }
     return _webView;
-}
-
-- (UIProgressView *)progressView{
-    if (!_progressView) {
-        _progressView = [[UIProgressView alloc] initWithProgressViewStyle:UIProgressViewStyleDefault];
-        // 设置进度条颜色
-        [_progressView setTrackTintColor:[UIColor whiteColor]];
-        _progressView.progressTintColor = [UIColor setHexColor:@"#76EE00"];
-    }
-    return _progressView;
 }
 
 - (UIButton *)closeBtn{
